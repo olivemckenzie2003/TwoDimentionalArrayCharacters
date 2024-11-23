d825e6173a182b193855632683fb28408bbefb8a
Olive Mckenzie <olivemckenzie2003@yahoo.com>
2024-11-23 05:18:21 +0000

    Update twoDimentionalArarryChars.cpp

--- twoDimentionalArarryChars.cpp
+++ twoDimentionalArarryChars.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(){
-
-	
-    char members1 [][name_length] {
+int main() {
+    
+    string members1[] {
         "John",
         "Samuel",
         "Rashid",
         "Rodriguez"
     };
-    
-    //Printing out members1
-    std::cout << "Printing out members1 (C-string literals) : " << std::endl;
-    for (size_t i {0}; i < std::size(members1) ; ++i){
-        std::cout << members1[i] << std::endl;
+
+    // Printing out members1
+    cout << "Printing out members1:" << endl;
+    for (const auto& member : members1) {
+        cout << member << endl;
     }
-    */
-   
 
-
-    //Updating our prediction declaration in the fortune teller game
-    char predictions [] [90] {
+    // Updating predictions
+    string predictions[] {
         "a lot of kinds running in the backyard!",
-        "a lot of empty beer bootles on your work table.",
+        "a lot of empty beer bottles on your work table.",
         "you Partying too much with kids wearing weird clothes.",
         "you running away from something really scary",
         "clouds gathering in the sky and an army standing ready for war",
@@ -30,15 +28,12 @@ int main(){
         "a lot of cars stuck in a terrible traffic jam",
         "you sitting in the dark typing lots of lines of code on your dirty computer",
         "you yelling at your boss. And oh no! You get fired!",
-        "you laughing your lungs out. I've never seen this before." ,
-        "Uhm , I don't see anything!"
-        
+        "you laughing your lungs out. I've never seen this before.",
+        "Uhm, I don't see anything!"
     };
-    
-	std::cout << std::endl;
-    std::cout << "Prediction : " << predictions[5] << std::endl;
- 
 
-   
-    return 0;
+    cout << endl;
+    cout << "Prediction: " << predictions[5] <<endl;
+
+    return 0; 
 }
